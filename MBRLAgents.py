73c406d596b8268c8d8cdfec1313a879f5a12d25
khayhamz31 <khayhafiz31@gmail.com>
2024-04-30 06:38:06 +0200

    exceot for ps

--- MBRLAgents.py
+++ MBRLAgents.py
@@ -17,11 +17,11 @@ class DynaAgent:
         self.n_actions = n_actions
         self.learning_rate = learning_rate
         self.gamma = gamma
-        self.Q_sa = np.zeros((n_states,n_actions))
-        self.transition = np.zeros((n_states,n_actions,n_states))
-        self.rewards = np.zeros((n_states,n_actions,n_states))
-        self.transition_estimate = np.zeros((self.n_states,self.n_actions,self.n_states))
-        self.reward_estimate = np.zeros((self.n_states,self.n_actions,self.n_states))
+        self.Q_sa = np.zeros((n_states,n_actions)) # intialise the q table
+        self.transition = np.zeros((n_states,n_actions,n_states)) # transition count
+        self.rewards = np.zeros((n_states,n_actions,n_states)) # reward sum 
+        self.transition_estimate = np.zeros((self.n_states,self.n_actions,self.n_states)) # transition estimate
+        self.reward_estimate = np.zeros((self.n_states,self.n_actions,self.n_states)) # reward estimate
         
     def select_action(self, s, epsilon):
         if np.random.rand() < epsilon:
@@ -31,22 +31,30 @@ class DynaAgent:
         return a
         
     def update(self,s,a,r,done,s_next,n_planning_updates):
+        # update model 
         self.transition[s][a][s_next] += 1 # update transition
         self.rewards[s][a][s_next] += r # update reward
-        
+        # update estimate transition
         count = np.sum(self.transition[s][a])
         for i in range(self.n_states):
             self.transition_estimate[s][a][i] = self.transition[s][a][i] / count
-        
+        # update estimate reward
         self.reward_estimate[s][a][s_next] =  self.rewards[s][a][s_next] / self.transition[s][a][s_next]
-
-        self.Q_sa[s][a] += self.learning_rate * (r + self.gamma * max(self.Q_sa[s_next]) - self.Q_sa[s][a]) # update q_table
-
+        # update Q table 
+        if not done: 
+            self.Q_sa[s][a] += self.learning_rate * (r + self.gamma * max(self.Q_sa[s_next]) - self.Q_sa[s][a]) # update q_table
+        else: 
+            self.Q_sa[s][a] += self.learning_rate * (r - self.Q_sa[s][a])
+        # for each plan 
         for _ in range(n_planning_updates):
-            state = np.random.choice(np.where(np.sum(self.transition, axis=(1, 2)) > 0)[0]) # find state previously observed
-            action = np.random.choice(np.where(self.transition[state].sum(axis=1) > 0)[0]) # find previously observed action in state
-            new_state = np.random.choice(range(self.n_states),p=self.transition_estimate[state][action]) # simulate s,a pair using transition estimate
+            visit_count = np.sum(self.transition.sum(axis=2), axis=1)
+            probs = visit_count / np.sum(visit_count)
+            state = np.random.choice(self.n_states, p=probs)
+            action_count = np.sum(self.transition[state], axis=1)
+            a_probs = action_count / np.sum(action_count)
+            action = np.random.choice(self.n_actions, p=a_probs)
 
+            new_state = np.random.choice(self.n_states,p=self.transition_estimate[state][action]) # simulate s,a pair using transition estimate
             new_r = self.reward_estimate[state][action][new_state] 
             self.Q_sa[state][action] += self.learning_rate * (new_r + self.gamma * max(self.Q_sa[new_state]) - self.Q_sa[state][action])
 
@@ -67,6 +75,7 @@ class DynaAgent:
         mean_return = np.mean(returns)
         return mean_return
 
+
 class PrioritizedSweepingAgent:
 
     def __init__(self, n_states, n_actions, learning_rate, gamma, max_queue_size=200, priority_cutoff=0.01):
@@ -78,8 +87,8 @@ class PrioritizedSweepingAgent:
         self.Q_sa = np.zeros((n_states,n_actions)) # initialise q_table
         self.transition = np.zeros((n_states, n_actions, n_states)) # intialise transtiion counts
         self.rewards = np.zeros((n_states, n_actions, n_states)) # initialise cumulative rewards
-        self.queue = PriorityQueue()
-        self.max_queue_size = max_queue_size
+        self.queue = PriorityQueue() # initalise priority queue
+        self.max_queue_size = max_queue_size # initialise max queue 
         self.transition_estimate = np.zeros((self.n_states,self.n_actions,self.n_states))
         self.reward_estimate = np.zeros((self.n_states,self.n_actions,self.n_states))
         
@@ -91,25 +100,27 @@ class PrioritizedSweepingAgent:
         return a
         
     def update(self,s,a,r,done,s_next,n_planning_updates):
-        self.transition[s][a][s_next] += 1 # update transition counts
-        self.rewards[s][a][s_next]+= r # update cumulative rewarsds
-
+        # update model 
+        self.transition[s][a][s_next] += 1 # update transition
+        self.rewards[s][a][s_next] += r # update reward
+        # update estimate transition
         count = np.sum(self.transition[s][a])
         for i in range(self.n_states):
-            self.transition_estimate[s][a][i] = self.transition[s][a][i] / count # update estimate transition
-        
-        self.reward_estimate[s][a][s_next] =  self.rewards[s][a][s_next] / self.transition[s][a][s_next] # update estimate reward
+            self.transition_estimate[s][a][i] = self.transition[s][a][i] / count
+        # update estimate reward
+        self.reward_estimate[s][a][s_next] =  self.rewards[s][a][s_next] / self.transition[s][a][s_next]
 
-        p_score = np.abs(r + self.gamma * max(self.Q_sa[s_next])- self.Q_sa[s][a]) # compute priority 
+        p_score = abs(r + self.gamma * np.max(self.Q_sa[s_next])- self.Q_sa[s][a]) # compute priority 
 
         if p_score > self.priority_cutoff and self.queue.qsize() < self.max_queue_size:
             self.queue.put((-p_score,(s,a))) # added to priority queue if met 
-        print(p_score)
         for _ in range(n_planning_updates): 
+            if self.queue.empty:
+                break
             score, pair = self.queue.get()
             state = pair[0]
             action = pair[1]
-            new_state = np.random.choice(range(self.n_states),p = self.transition_estimate[state][action])
+            new_state = np.random.choice(self.n_states,p = self.transition_estimate[state][action])
             new_r = self.reward_estimate[state][action][new_state]
             self.Q_sa[state][action] += self.learning_rate * (new_r + (self.gamma * max(self.Q_sa[new_state])-self.Q_sa[state][action]))
             for i in range(self.n_states):
@@ -120,6 +131,23 @@ class PrioritizedSweepingAgent:
                             p_1 = np.abs(r_1 + self.gamma * max(self.Q_sa[state])-self.Q_sa[i][act])
                             if p_1 > self.priority_cutoff and self.queue.qsize() < self.max_queue_size:
                                 self.queue.put((-p_1,(i,act)))
+
+    def evaluate(self, eval_env, n_eval_episodes=30, max_episode_length=100):
+        returns = []  # list to store the reward per episode
+        for i in range(n_eval_episodes):
+            s = eval_env.reset()
+            R_ep = 0
+            for t in range(max_episode_length):
+                a = np.argmax(self.Q_sa[s])  # greedy action selection
+                s_prime, r, done = eval_env.step(a)
+                R_ep += r
+                if done:
+                    break
+                else:
+                    s = s_prime
+            returns.append(R_ep)
+        mean_return = np.mean(returns)
+        return mean_return
 
 
     def evaluate(self,eval_env,n_eval_episodes=30, max_episode_length=100):
@@ -146,8 +174,8 @@ def test():
 
     # Algorithm parameters
 
-    # policy = 'dyna'
-    policy = 'ps'
+    policy = 'dyna'
+    # policy = 'ps'
     # policy = 'dyna' or 'ps' 
     epsilon = 0.1
     learning_rate = 0.2
@@ -160,6 +188,7 @@ def test():
     
     # Initialize environment and policy
     env = WindyGridworld()
+    print(env.n_states)
     if policy == 'dyna':
         pi = DynaAgent(env.n_states,env.n_actions,learning_rate,gamma) # Initialize Dyna policy
     elif policy == 'ps':    
--- MBRLExperiment.py
+++ MBRLExperiment.py
@@ -10,26 +10,70 @@ import numpy as np
 from MBRLEnvironment import WindyGridworld
 from MBRLAgents import DynaAgent, PrioritizedSweepingAgent
 from Helper import LearningCurvePlot, smooth
+import os 
 
 
+def run_repetitions(age,wind_proportions,n_planning_updates,n_repetitions,n_timesteps,eval_interval,learning_rate,gamma,epsilon):
+    results = np.zeros((len(wind_proportions),len(n_planning_updates),n_repetitions,n_timesteps//eval_interval))
+    for windex, wind in enumerate(wind_proportions):
+        for plandex, plan in enumerate(n_planning_updates):
+            for rep in range(n_repetitions):
+                env = WindyGridworld(wind_proportion=wind)
+                if age == 'd':
+                    agent = DynaAgent(env.n_states,env.n_actions,learning_rate,gamma)
+                current = env.reset()
+                returns = np.zeros(n_timesteps//eval_interval)
+                for idx in range(n_timesteps):
+                    if (idx + 1) % eval_interval == 0:
+                        eval = agent.evaluate(env)
+                        index = (idx + 1) // eval_interval - 1 
+                        returns[index] += eval 
+                    act = agent.select_action(current,epsilon)
+                    s_next,r,done = env.step(act)
+                    agent.update(s=current,a=act,r=r,done=done,s_next=s_next,n_planning_updates=plan)
+                    if done:
+                        current = env.reset()
+                    else:
+                        current = s_next
+                results[windex][plandex][rep] = returns 
+    np.save(f'{age}_{wind_proportions[0]}.npy',results)
+    return results
 
-
-
-def plot_results(result_array, wind_proportions, n_planning_updates, name="plot", baseline=None, labels=None):
-    plot = LearningCurvePlot()
-    for i, wind in enumerate(wind_proportions):
-        for j, n_updates in enumerate(n_planning_updates):
-            if labels is None:
-                lbl = f"wind: {wind}, n_updates: {n_updates}"
+def run_repetitions_ps(n_timesteps, n_repetitions, eval_interval, gamma, learning_rate, epsilon, n_planning_update, wind_proportion):
+    result = np.zeros((n_repetitions,n_timesteps//eval_interval))
+    for reps in range(n_repetitions): 
+        env = WindyGridworld(wind_proportion=wind_proportion[0])
+        agent = PrioritizedSweepingAgent(env.n_states,env.n_actions,learning_rate,gamma,)
+        current = env.reset()
+        returns = np.zeros(n_timesteps//eval_interval)
+        for idx in range(n_timesteps):
+            if (idx + 1) % eval_interval == 0:
+                eval = agent.evaluate(env)
+                index = (idx + 1) // eval_interval - 1 
+                returns[index] += eval 
+            act = agent.select_action(current,epsilon)
+            s_next,r,done = env.step(act)
+            agent.update(s=current,a=act,r=r,done=done,s_next=s_next,n_planning_updates=plan)
+            if done:
+                current = env.reset()
             else:
-                lbl = labels[i + j]
-            plot.add_curve(x=range(len(result_array[0, 0, :])), y=smooth(result_array[i, j], window=10),
-                           label=lbl)
-        if baseline is not None:
-            plot.add_curve(x=range(len(result_array[0, 0, :])), y=smooth(baseline[i, 0], window=10),
-                           label=f"QLearning, wind: {wind}")
-    plot.save(name=f"{name}.png")
-
+                current = s_next
+        result[reps] = returns
+def graphs(results, agent_type, env_type,updates,n_timesteps,eval_interval):
+        LCP = LearningCurvePlot()
+        mean  = np.mean(results,axis=2)
+        if agent_type == 'd':
+            agent_t = 'dyna'
+        else: 
+            agent_t = 'Prioritized sweeping'
+        if env_type == 0:
+            evn_t = 'stochastic'
+        else: 
+            evn_t = 'deterministic'
+        for idx,update in enumerate(updates):
+            smoothed = smooth(mean[0][idx],window = 11)
+            LCP.add_curve(range(n_timesteps//eval_interval), smoothed, label=f"{evn_t} environment, updates: {update}")
+        LCP.save(f'{agent_t},{evn_t}')
 
 def experiment():
     n_timesteps = 10001
@@ -37,94 +81,27 @@ def experiment():
     n_repetitions = 10
     gamma = 1.0
     learning_rate = 0.2
-    epsilon = 0.1
+    epsilon=0.1
 
-    wind_proportions = [0.9, 1.0]
-    n_planning_updatess = [1, 3, 5]
+    wind_proportions=[0.9,1.0]
+    n_planning_updates = [0,1,3,5] 
 
-    max_episode_length = 100
-
-    def run_repetitions(n_repetitions, n_timesteps, eval_interval, gamma, learning_rate, epsilon, wind_proportions, n_planning_updates, max_episode_length, model="Dyna"):
-        results = np.zeros((len(wind_proportions), len(n_planning_updates), n_timesteps // eval_interval))
-
-        for windex, wind in enumerate(wind_proportions):
-            for planex, plan in enumerate(n_planning_updates):
-                for rep in range(n_repetitions):
-                    env = WindyGridworld()
-                    if model == 'Dyna':
-                        agent =  DynaAgent(env.n_states, env.n_actions, learning_rate, gamma)
-                    else: 
-                        agent = PrioritizedSweepingAgent(env.n_states, env.n_actions, learning_rate, gamma)
-
-                    s = env.reset()
-                    rewards = []
-
-                    for step in range(n_timesteps):
-                        if (step + 1) % eval_interval == 0:
-                            eval = agent.evaluate(env, max_episode_length=max_episode_length)
-                            rewards.append(eval )
-
-                        a = agent.select_action(s, epsilon)
-                        s_prime, r, done = env.step(a)
-                        agent.update(s, a, r, done, s_i, plan)
-                        if done:
-                            s = env.reset()
-                        else:
-                            s = s_i
-
-                    results[windex, planex, :] += np.array(rewards) / n_repetitions
-
-        return results
-
-    results = run_repetitions(n_repetitions, n_timesteps, eval_interval, gamma, learning_rate, epsilon,
-                              [0], [1], max_episode_length, "Dyna")
-    plot_results(results, [0], [1], name="1b")
-
-    # 1c
-    for wind in wind_proportions:
-        q_learning_baseline = run_repetitions(n_repetitions, n_timesteps, eval_interval, gamma, learning_rate, epsilon,
-                                              [wind], [0], max_episode_length, "Dyna")
-        results = run_repetitions(n_repetitions, n_timesteps, eval_interval, gamma, learning_rate, epsilon,
-                                  [wind], n_planning_updatess, max_episode_length, "Dyna")
-        plot_results(results, [wind], n_planning_updatess, name=f"1c_wind{wind}", baseline=q_learning_baseline)
-
-    # 2b
-    results = run_repetitions(n_repetitions, n_timesteps, eval_interval, gamma, learning_rate, epsilon,
-                              [0], [1], max_episode_length, "SW")
-    plot_results(results, [0], [1], name="2b")
-
-    # 2c
-    for wind in wind_proportions:
-        q_learning_baseline = run_repetitions(n_repetitions, n_timesteps, eval_interval, gamma, learning_rate, epsilon,
-                                              [wind], [0], max_episode_length, "Dyna")
-        results = run_repetitions(n_repetitions, n_timesteps, eval_interval, gamma, learning_rate, epsilon,
-                                  [wind], n_planning_updatess, max_episode_length, "SW")
-        plot_results(results, [wind], n_planning_updatess, name=f"2c_wind{wind}", baseline=q_learning_baseline)
-
-    # 3
-    for wind in wind_proportions:
-        q_learning_baseline = run_repetitions(n_repetitions, n_timesteps, eval_interval, gamma, learning_rate, epsilon,
-                                              [wind], [0], max_episode_length, "Dyna")
-
-        results = run_repetitions(n_repetitions, n_timesteps, eval_interval, gamma, learning_rate, epsilon,
-                                  [wind], n_planning_updatess, max_episode_length, "Dyna")
-        best_dyna_index = np.argmax(np.mean(results[0], axis=1))
-        best_performing_dyna = results[0][best_dyna_index]
-
-        results = run_repetitions(n_repetitions, n_timesteps, eval_interval, gamma, learning_rate, epsilon,
-                                  [wind], n_planning_updatess, max_episode_length, "SW")
-        best_sw_index = np.argmax(np.mean(results[0], axis=1))
-        best_performing_sw = results[0][best_sw_index]
-
-        best_performers = np.stack((best_performing_dyna, best_performing_sw))
-        best_performers = best_performers.reshape(1, best_performers.shape[0], best_performers.shape[1])
-
-        plot_results(best_performers, [wind],
-                     [n_planning_updatess[best_dyna_index], n_planning_updatess[best_sw_index]], name=f"3_wind{wind}",
-                     baseline=q_learning_baseline,
-                     labels=[f"Best performing Dyna with n_updates: {n_planning_updatess[best_dyna_index]}",
-                             f"Best performing SW with n_updates: {n_planning_updatess[best_sw_index]}"])
-
+    # dd = run_repetitions('d',[1],n_planning_updates = n_planning_updates,n_repetitions = n_repetitions ,n_timesteps = n_timesteps ,eval_interval = eval_interval,learning_rate = learning_rate,gamma = gamma,epsilon = epsilon)
+    # ds = run_repetitions('d',[0.9],n_planning_updates = n_planning_updates,n_repetitions = n_repetitions ,n_timesteps = n_timesteps ,eval_interval = eval_interval,learning_rate = learning_rate,gamma = gamma,epsilon = epsilon)
+    pd = run_repetitions('ps',[1],n_planning_updates = n_planning_updates,n_repetitions = n_repetitions ,n_timesteps = n_timesteps ,eval_interval = eval_interval,learning_rate = learning_rate,gamma = gamma,epsilon = epsilon)
+    ps = run_repetitions('ps',[0.9],n_planning_updates = n_planning_updates,n_repetitions = n_repetitions ,n_timesteps = n_timesteps ,eval_interval = eval_interval,learning_rate = learning_rate,gamma = gamma,epsilon = epsilon)
+    # dd = np.load("d_1.npy")
+    # ds = np.load("d_0.9.npy")
+    # pd = np.load("ps_1.npy")
+    # ps = np.load("ps_0.9.npy")
+    # print(pd)
+    # print(ps)
+    # print(pd)
+    # print(ps)
+    # graphs(dd, 'd', 1,n_planning_updates,n_timesteps,eval_interval)
+    # graphs(ds, 'd', 0,n_planning_updates,n_timesteps,eval_interval)
+    graphs(pd, 'p', 1,n_planning_updates,n_timesteps,eval_interval)
+    graphs(ps, 'p', 0,n_planning_updates,n_timesteps,eval_interval)
 
 if __name__ == '__main__':
     experiment()
