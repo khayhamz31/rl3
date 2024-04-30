fede4c1f3deb7b173f129156eb35b23e3e645c11
khayhamz31 <160015441+khayhamz31@users.noreply.github.com>
2024-04-30 12:28:42 +0200

    k last exp

--- MBRLExperiment.py
+++ MBRLExperiment.py
@@ -10,7 +10,7 @@ import numpy as np
 from MBRLEnvironment import WindyGridworld
 from MBRLAgents import DynaAgent, PrioritizedSweepingAgent
 from Helper import LearningCurvePlot, smooth
-import os 
+import os
 
 
 def run_repetitions(age,wind_proportions,n_planning_updates,n_repetitions,n_timesteps,eval_interval,learning_rate,gamma,epsilon):
@@ -21,13 +21,15 @@ def run_repetitions(age,wind_proportions,n_planning_updates,n_repetitions,n_time
                 env = WindyGridworld(wind_proportion=wind)
                 if age == 'd':
                     agent = DynaAgent(env.n_states,env.n_actions,learning_rate,gamma)
+                else:
+                    agent = PrioritizedSweepingAgent(env.n_states,env.n_actions,learning_rate,gamma)
                 current = env.reset()
                 returns = np.zeros(n_timesteps//eval_interval)
                 for idx in range(n_timesteps):
                     if (idx + 1) % eval_interval == 0:
                         eval = agent.evaluate(env)
-                        index = (idx + 1) // eval_interval - 1 
-                        returns[index] += eval 
+                        index = (idx + 1) // eval_interval - 1
+                        returns[index] += eval
                     act = agent.select_action(current,epsilon)
                     s_next,r,done = env.step(act)
                     agent.update(s=current,a=act,r=r,done=done,s_next=s_next,n_planning_updates=plan)
@@ -35,40 +37,42 @@ def run_repetitions(age,wind_proportions,n_planning_updates,n_repetitions,n_time
                         current = env.reset()
                     else:
                         current = s_next
-                results[windex][plandex][rep] = returns 
+                results[windex][plandex][rep] = returns
     np.save(f'{age}_{wind_proportions[0]}.npy',results)
     return results
 
-def run_repetitions_ps(n_timesteps, n_repetitions, eval_interval, gamma, learning_rate, epsilon, n_planning_update, wind_proportion):
-    result = np.zeros((n_repetitions,n_timesteps//eval_interval))
-    for reps in range(n_repetitions): 
-        env = WindyGridworld(wind_proportion=wind_proportion[0])
-        agent = PrioritizedSweepingAgent(env.n_states,env.n_actions,learning_rate,gamma,)
-        current = env.reset()
-        returns = np.zeros(n_timesteps//eval_interval)
-        for idx in range(n_timesteps):
-            if (idx + 1) % eval_interval == 0:
-                eval = agent.evaluate(env)
-                index = (idx + 1) // eval_interval - 1 
-                returns[index] += eval 
-            act = agent.select_action(current,epsilon)
-            s_next,r,done = env.step(act)
-            agent.update(s=current,a=act,r=r,done=done,s_next=s_next,n_planning_updates=plan)
-            if done:
-                current = env.reset()
-            else:
-                current = s_next
-        result[reps] = returns
+# def run_repetitions_ps(n_timesteps, n_repetitions, eval_interval, gamma, learning_rate, epsilon, n_planning_update, wind_proportion):
+#     result = np.zeros((n_repetitions,n_timesteps//eval_interval))
+#     for reps in range(n_repetitions):
+#         env = WindyGridworld(wind_proportion=wind_proportion[0])
+#         agent = PrioritizedSweepingAgent(env.n_states,env.n_actions,learning_rate,gamma,)
+#         current = env.reset()
+#         returns = np.zeros(n_timesteps//eval_interval)
+#         for idx in range(n_timesteps):
+#             if (idx + 1) % eval_interval == 0:
+#                 eval = agent.evaluate(env)
+#                 index = (idx + 1) // eval_interval - 1
+#                 returns[index] += eval
+#             act = agent.select_action(current,epsilon)
+#             s_next,r,done = env.step(act)
+#             agent.update(s=current,a=act,r=r,done=done,s_next=s_next,n_planning_updates=plan)
+#             if done:
+#                 current = env.reset()
+#             else:
+#                 current = s_next
+#         result[reps] = returns
+
+
 def graphs(results, agent_type, env_type,updates,n_timesteps,eval_interval):
         LCP = LearningCurvePlot()
         mean  = np.mean(results,axis=2)
         if agent_type == 'd':
             agent_t = 'dyna'
-        else: 
+        else:
             agent_t = 'Prioritized sweeping'
         if env_type == 0:
             evn_t = 'stochastic'
-        else: 
+        else:
             evn_t = 'deterministic'
         for idx,update in enumerate(updates):
             smoothed = smooth(mean[0][idx],window = 11)
@@ -84,22 +88,19 @@ def experiment():
     epsilon=0.1
 
     wind_proportions=[0.9,1.0]
-    n_planning_updates = [0,1,3,5] 
+    n_planning_updates = [0,1,3,5]
 
-    # dd = run_repetitions('d',[1],n_planning_updates = n_planning_updates,n_repetitions = n_repetitions ,n_timesteps = n_timesteps ,eval_interval = eval_interval,learning_rate = learning_rate,gamma = gamma,epsilon = epsilon)
-    # ds = run_repetitions('d',[0.9],n_planning_updates = n_planning_updates,n_repetitions = n_repetitions ,n_timesteps = n_timesteps ,eval_interval = eval_interval,learning_rate = learning_rate,gamma = gamma,epsilon = epsilon)
+    dd = run_repetitions('d',[1],n_planning_updates = n_planning_updates,n_repetitions = n_repetitions ,n_timesteps = n_timesteps ,eval_interval = eval_interval,learning_rate = learning_rate,gamma = gamma,epsilon = epsilon)
+    ds = run_repetitions('d',[0.9],n_planning_updates = n_planning_updates,n_repetitions = n_repetitions ,n_timesteps = n_timesteps ,eval_interval = eval_interval,learning_rate = learning_rate,gamma = gamma,epsilon = epsilon)
     pd = run_repetitions('ps',[1],n_planning_updates = n_planning_updates,n_repetitions = n_repetitions ,n_timesteps = n_timesteps ,eval_interval = eval_interval,learning_rate = learning_rate,gamma = gamma,epsilon = epsilon)
     ps = run_repetitions('ps',[0.9],n_planning_updates = n_planning_updates,n_repetitions = n_repetitions ,n_timesteps = n_timesteps ,eval_interval = eval_interval,learning_rate = learning_rate,gamma = gamma,epsilon = epsilon)
     # dd = np.load("d_1.npy")
     # ds = np.load("d_0.9.npy")
     # pd = np.load("ps_1.npy")
     # ps = np.load("ps_0.9.npy")
-    # print(pd)
-    # print(ps)
-    # print(pd)
-    # print(ps)
-    # graphs(dd, 'd', 1,n_planning_updates,n_timesteps,eval_interval)
-    # graphs(ds, 'd', 0,n_planning_updates,n_timesteps,eval_interval)
+
+    graphs(dd, 'd', 1,n_planning_updates,n_timesteps,eval_interval)
+    graphs(ds, 'd', 0,n_planning_updates,n_timesteps,eval_interval)
     graphs(pd, 'p', 1,n_planning_updates,n_timesteps,eval_interval)
     graphs(ps, 'p', 0,n_planning_updates,n_timesteps,eval_interval)
 
